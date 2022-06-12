7265fc8389fe57bb1acde5befd5f12d9e8aec843
Rochev Alexandr <velloso3@yandex.ru>
2022-06-12 23:54:21 +0300

    add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,21 @@ import './styles/main.scss'
 
 function App() {
   return (
-    <div className='container'>
-      <h1>Hello!</h1>
+    <div style={{
+      color: '#fabc3b',
+      backgroundColor: '#1a3866',
+      height: '100vh',
+    }}
+         className='container'>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, aliquam
+         aut et illum impedit in ipsa maiores minima numquam perspiciatis
+         placeat porro possimus praesentium quia recusandae soluta suscipit
+         voluptas voluptatibus.</p>
+
+      <p style={{ fontFamily: 'Neris Black' }}>Hello! Привет!</p>
+      <p style={{ fontFamily: 'Nougat' }}>Hello! Привет!</p>
+      <p style={{ fontFamily: 'Lilita One' }}>Hello! Привет!</p>
     </div>
   )
 }
